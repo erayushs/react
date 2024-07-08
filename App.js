feccbaafd72b065618397719467ad5fec3e95709
Ayush Singh <anmol8960@gmail.com>
2024-07-08 12:30:48 +0530

    Parcel update

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const ele = React.createElement("h1", { id: "h1Ele" }, "Hello from React 🚀");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
